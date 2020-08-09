92d830b899de7125b402e06c2d69b071ad63a5c5
rajan savaliya <rajanpatel99094@gmail.com>
2020-08-09 15:40:26 +0530

    Updates

---
+++ precache-manifest.91ddad94128a0e7885fa86f79b5495cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c456d38facb0673ec28df899947f132",
+    "url": "/covid-19-react-deploy/index.html"
+  },
+  {
+    "revision": "f0778bd1f10b799a98fb",
+    "url": "/covid-19-react-deploy/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "c148b55661a0b4db38d8",
+    "url": "/covid-19-react-deploy/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "f0778bd1f10b799a98fb",
+    "url": "/covid-19-react-deploy/static/js/2.741906ff.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/covid-19-react-deploy/static/js/2.741906ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c148b55661a0b4db38d8",
+    "url": "/covid-19-react-deploy/static/js/main.967cbff4.chunk.js"
+  },
+  {
+    "revision": "16810da618bd380322fb",
+    "url": "/covid-19-react-deploy/static/js/runtime-main.6d69c2ee.js"
+  }
+]);
